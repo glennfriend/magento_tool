4ab712d23e10febdd77fa8b9183dc1a8a55a864d
turtlemt <turtlemt@gmail.com>
2014-06-19 14:17:54 +0800

    Update comment

--- config/config.template.php
+++ config/config.template.php
@@ -1,5 +1,11 @@
 <?php
 
+    /*
+        v1
+            /api/soap/?wsdl=1
+        v2
+            /api/v2_soap/?wsdl=1
+    */
     define('MAGENTO_API_URL', 'https://xxxxxxxxxx/api/soap/?wsdl=1' );
     define('MAGENTO_API_NAME', 'xxxxxxxxxx' );
     define('MAGENTO_API_KEY', 'xxxxxxxxxx' );
